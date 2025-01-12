8abc9c6a768e26a470cc632f0c5bd05af892ae21
Kostiancheck <gor.kosty@gmail.com>
2025-01-12 18:02:04 +0200

    WebGL for dice rotation, some changes

--- programming/webgl/4-final-dice-animation.js
+++ programming/webgl/4-final-dice-animation.js
@@ -20,15 +20,24 @@ const oZ = new Vector3(0, 0, 1)
 
 // Light
 const light = new THREE.DirectionalLight(0xffffff, 100);
-light.position.set(-20, 300, 10);
+light.position.set(-100, 300, 300);
 scene.add(light);
 
 const light2 = new THREE.DirectionalLight(0xffffff, 20);
-light2.position.set(20, -300, 200);
+light2.position.set(300, -300, 200);
 scene.add(light2);
 
-const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
-scene.add(ambLight);
+const light3 = new THREE.DirectionalLight(0xffffff, 20);
+light3.position.set(-300, -300, 200);
+scene.add(light3);
+
+// const lightHelper = new THREE.DirectionalLightHelper(light, 10);
+// scene.add(lightHelper);
+// const lightHelper2 = new THREE.DirectionalLightHelper(light2, 10);
+// scene.add(lightHelper2);
+
+// const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
+// scene.add(ambLight);
 
 let model = new THREE.Object3D();
 const loader = new GLTFLoader();
@@ -36,9 +45,10 @@ loader.load('d20_dice_w20_wurfel_3d_model_free/scene.gltf', (gltf) => {
     model = gltf.scene;
     // model.rotateX(degToRad(30));
     // model.rotateZ(degToRad(180));
+    console.log(model.rotation.x)
     model.rotateOnWorldAxis(oX, degToRad(45))
     model.rotateOnWorldAxis(oZ, degToRad(180))
-    // // model.rotation.x = 0.3
+    console.log(model.rotation.x)
     scene.add(model);
 }, undefined, (error) => {
     console.error('An error occurred loading the model:', error);
@@ -51,6 +61,7 @@ controls.screenSpacePanning = false;
 controls.minDistance = 1;
 controls.maxDistance = 5000;
 
+console.log()
 // Load background
 const backLoader = new THREE.TextureLoader();
 backLoader.load('random_back.jpg', function (texture) {
@@ -61,45 +72,59 @@ backLoader.load('random_back.jpg', function (texture) {
 camera.position.set(0, 0, 500);
 
 // Create a TWEEN.Group to manage the animations
-const tweenGroup = new TWEEN.Group();
+// const tweenGroup = new TWEEN.Group();
 
 // Create the tween animation for position and rotation
 // const createRotationTween = () => {
-//     return new TWEEN.Tween(
-//         {
-//             xRotation: model.rotation.x,
-//             yRotation: model.rotation.y,
-//             zRotation: model.rotation.z
-//         }, tweenGroup)
-//         .to({
-//             xRotation: model.rotation.x + Math.PI * 7,
-//             yRotation: model.rotation.y + Math.PI * 7,
-//             zRotation: model.rotation.z + Math.PI * 7
-//         }, 10000)
+//     return new TWEEN.Tween(model, tweenGroup)
 //         .onUpdate((coords) => {
 //             // Update rotation smoothly
-//             model.rotation.x = coords.xRotation;
-//             model.rotation.y = coords.yRotation;
-//             model.rotation.z = coords.zRotation;
+//             model.rotateOnWorldAxis(oX, degToRad(180));
+//             model.rotateOnWorldAxis(oY, degToRad(180));
+//             model.rotateOnWorldAxis(oZ, degToRad(180));
 //         })
 //         .easing(TWEEN.Easing.Sinusoidal.InOut)
-//         .onComplete(function () {
-//             createRotationTween().start();
-//         })
 // };
-//
-//
-// // Start the first tween
+
+
+// Start the first tween
 // createRotationTween().start();
 
+
+function xRotation(t) {
+    return degToRad(2)
+}
+
+function yRotation(t) {
+    return degToRad(4 * Math.cos(t/2000+200))
+}
+
+function zRotation(t) {
+    return degToRad(2 * Math.sin(t/2000))
+}
+
 // Animation Loop
-const animate = () => {
-    requestAnimationFrame(animate);
+const animate = (t) => {
+    let id = requestAnimationFrame(animate);
     // Update the tween group instead of using the deprecated TWEEN.update()
-    tweenGroup.update();
+    if (t > 10000){
+        cancelAnimationFrame(id)
+    }
+
+    model.rotateOnWorldAxis(oX, xRotation(t));
+    console.log("X =" + model.rotation.x)
+    model.rotateOnWorldAxis(oY, yRotation(t));
+    console.log("Y =" + model.rotation.y)
+    model.rotateOnWorldAxis(oZ, zRotation(t));
+    console.log("Z =" + model.rotation.z)
+    // console.log(Math.sin(t))
+
+    // model.rotation.x += degToRad(1);
+    // model.rotation.y += degToRad(1);
+    // model.rotation.z += degToRad(1);
     renderer.render(scene, camera);
 }
-animate();
+animate(0);
 
 // Handle window resize
 window.addEventListener('resize', () => {
